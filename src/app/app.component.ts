5dcf45f759f1613153b3e0703ab95ef5fc5ed788
Aldus Daniel (AldusD) <aldusdanielfsm@gmail.com>
2025-06-26 00:50:51 -0300

    feat: starts project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { LandingComponent } from './landing/landing.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [LandingComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {}
