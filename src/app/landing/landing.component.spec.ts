9bf4ed454fce945c1745459c23a2b2dc9a58baf7
Aldus Daniel (AldusD) <aldusdanielfsm@gmail.com>
2025-06-26 01:15:23 -0300

    feat: adds tests

--- src/app/landing/landing.component.spec.ts
+++ src/app/landing/landing.component.spec.ts
@@ -5,4 +5,19 @@ import { LandingComponent } from './landing.component';
 describe('Landing', () => {
   let component: LandingComponent;
   let fixture: ComponentFixture<LandingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LandingComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LandingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('This test will always pass', () => {
+    expect(true).toBeTruthy();
+  });
 });
