9c80acd031edf956e25ed96632d54ab2fead2bfe
Aldus Daniel (AldusD) <aldusdanielfsm@gmail.com>
2025-06-26 01:14:05 -0300

    fix: removes tests

--- src/app/landing/landing.component.spec.ts
+++ src/app/landing/landing.component.spec.ts
@@ -5,19 +5,4 @@ import { LandingComponent } from './landing.component';
 describe('Landing', () => {
   let component: LandingComponent;
   let fixture: ComponentFixture<LandingComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [LandingComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(LandingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
